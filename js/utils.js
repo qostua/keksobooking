02e48ca112e366da38bcbec3dfca983ad813caff
qostua <qostua@gmail.com>
2022-10-07 18:04:47 +0300

    Удалил вспомогательные функции генерации случайных чисел

--- js/utils.js
+++ js/utils.js
@@ -1,17 +1,4 @@
-function getRandomPositiveInteger (val1, val2) {
-  const lower = Math.ceil(Math.min(Math.abs(val1), Math.abs(val2)));
-  const upper = Math.floor(Math.max(Math.abs(val1), Math.abs(val2)));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-}
-function getRandomPositiveFloat (val1, val2, digits = 1) {
-  const lower = Math.min(Math.abs(val1), Math.abs(val2));
-  const upper = Math.max(Math.abs(val1), Math.abs(val2));
-  const result = Math.random() * (upper - lower) + lower;
-  return result.toFixed(digits);
-}
-
-function getNoun(number, oneNounValue, twoNounsValue, fiveNounsValue) {
+const getNoun = (number, oneNounValue, twoNounsValue, fiveNounsValue) => {
   number %= 100;
   if (number >= 5 && number <= 20) {
     return fiveNounsValue;
