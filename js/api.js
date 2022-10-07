2d58c58c272b971f12ecd182ba5a41cc94c17401
qostua <qostua@gmail.com>
2022-10-07 18:07:09 +0300

    Добавил модуль для взаимодействия с сервером

---
+++ js/api.js
@@ -0,0 +1,45 @@
+import {showAlert} from './utils.js';
+
+const Url = {
+  DATA: 'https://23.javascript.pages.academy/keksobooking/data',
+  SERVER: 'https://23.javascript.pages.academy/keksobooking',
+};
+
+const getData = (onSucces) => {
+  fetch(Url.DATA)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        showAlert('error-server');
+      }
+    })
+    .then((offers) => {
+      onSucces(offers);
+    })
+    .catch(() => {
+      showAlert('error-server');
+    });
+};
+
+const sendData = (onSucces, onFail, body) => {
+  fetch(
+    Url.SERVER,
+    {
+      method: 'POST',
+      body,
+    },
+  )
+    .then((response) => {
+      if (response.ok) {
+        onSucces();
+      } else {
+        onFail();
+      }
+    })
+    .catch(() => {
+      onFail();
+    });
+};
+
+export {getData, sendData};
