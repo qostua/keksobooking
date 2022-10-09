38f8077ae61b257ca15bde32ed2ace364e2bbf30
qostua <qostua@gmail.com>
2022-10-09 21:46:55 +0300

    В точку входа добавлена функция генерации/фильтрации попапов и дебаунс

--- js/main.js
+++ js/main.js
@@ -1,15 +1,45 @@
-import {setAdFormSubmit, resetForm, setAddressValue} from './form.js';
-import {activateAdForm, activateMapForm} from './activation-page.js';
-import {getData} from './api.js';
-import {createOffersList, getMap, setMainMarkerMove} from './map.js';
-import {generateOffersList} from './generate-card.js';
-import {showAlert} from './utils.js';
+import {
+  setAdFormSubmit,
+  resetForm,
+  setAddressValue
+} from './ad-form.js';
+import {
+  setMapFormChange
+} from './map-form.js';
+import {
+  activateAdForm,
+  activateMapForm
+}from './activation-page.js';
+import {
+  getData
+} from './api.js';
+import {
+  renderPopups,
+  getMap,
+  setMainMarkerMove
+} from './map.js';
+import {
+  generatePopupList
+} from './generate-popup-list.js';
+import {
+  showAlert,
+  debounce
+} from './utils.js';
+import './preview-image.js';
+
+const RENDER_DELAY = 500;
 
 getMap(() => {
   activateAdForm();
-  getData((offers) => {
-    createOffersList(generateOffersList(offers));
+  getData((adsData) => {
+    renderPopups(generatePopupList(adsData));
     activateMapForm();
+    setMapFormChange(
+      debounce(
+        () => renderPopups(generatePopupList(adsData)),
+      ),
+      RENDER_DELAY,
+    );
   });
   setMainMarkerMove(setAddressValue);
 });
