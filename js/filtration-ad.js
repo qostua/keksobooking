7dcc019150378a2c7c5a03cbbe6f4a7581a147d2
qostua <qostua@gmail.com>
2022-10-11 21:40:17 +0300

    Фильтрация объявлений теперь происходит корректно и более эффективно

---
+++ js/filtration-ad.js
@@ -0,0 +1,65 @@
+import {
+  isMapFormFiltersActive,
+  isMapFormFeaturesActive,
+  getActiveFiltersMapForm,
+  getActiveFeaturesMapForm
+} from './map-form.js';
+
+const MAX_ADS_COUNT = 10;
+
+const isAdOptionsMatch = (adOffer, filtersMapForm) => {
+  const {type, priceRange, rooms, guests} = filtersMapForm;
+
+  const isTypeMatch = !type || type === adOffer.type;
+  const isRoomsMatch = !rooms || rooms === String(adOffer.rooms);
+  const isGuestsMatch = !guests || guests === String(adOffer.guests);
+  const isPriceMatch = adOffer.price >= priceRange.start && adOffer.price < priceRange.end;
+
+  return isTypeMatch && isRoomsMatch && isGuestsMatch && isPriceMatch;
+};
+const isAdFeaturesMatch = (adOffer, mapFormfeatures) => {
+  const offerFeatures = adOffer.features;
+
+  if (!offerFeatures) {
+    return false;
+  }
+
+  const rating = offerFeatures.reduce((rating, feature) => {
+    if (mapFormfeatures.includes(feature)) {
+      rating++;
+    }
+    return rating;
+  }, 0);
+
+  return rating === mapFormfeatures.length;
+};
+
+const getFilteredAds = (adData) => {
+  if (!isMapFormFiltersActive() && !isMapFormFeaturesActive()) {
+    return adData.slice(0, MAX_ADS_COUNT);
+  }
+
+  const filteredAds = [];
+  const mapFormFilters = getActiveFiltersMapForm();
+  const mapFormfeatures = getActiveFeaturesMapForm();
+
+  for (let index = 0; index < adData.length; index++) {
+    if (isMapFormFiltersActive() && !isAdOptionsMatch(adData[index].offer, mapFormFilters)) {
+      continue;
+    }
+    if (isMapFormFeaturesActive() && !isAdFeaturesMatch(adData[index].offer, mapFormfeatures)) {
+      continue;
+    }
+
+    filteredAds.push(adData[index]);
+    if (filteredAds.length === MAX_ADS_COUNT) {
+      break;
+    }
+  }
+
+  return filteredAds;
+};
+
+export {
+  getFilteredAds
+};
