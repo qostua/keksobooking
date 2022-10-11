598dfcbe8ed635caa6c28d19cc6b2e84c326a447
qostua <qostua@gmail.com>
2022-10-11 21:30:33 +0300

    Изменил интерфейс модуля предпросмотра фотографий - теперь обработчики можно добавить из другого модуля.

--- js/preview-image.js
+++ js/preview-image.js
@@ -1,59 +1,78 @@
-import {removePreviewPhoto} from './ad-form.js';
-
 const FILE_TYPES = ['jpg', 'jpeg', 'png'];
+const DEFAULT_AVATAR_URL = 'img/muffin-grey.svg';
 
 const adForm = document.querySelector('.ad-form');
-
 const avatarChooser = adForm.querySelector('#avatar');
 const avatarPreview = adForm.querySelector('.ad-form-header__preview img');
-
 const adImageChooser = adForm.querySelector('#images');
-const adFormPhotoContainer = adForm.querySelector('.ad-form__photo-container');
+const adPhotoContainer = adForm.querySelector('.ad-form__photo-container');
 
 const isTypeMatche = (fileName) => FILE_TYPES.some((type) => fileName.endsWith(type));
-
-avatarChooser.addEventListener('change', (evt) => {
-  const file = evt.target.files[0];
-  const fileName = file.name.toLowerCase();
-
-  if (!isTypeMatche(fileName)) {
-    return;
-  }
-
-  const reader = new FileReader();
-
-  reader.addEventListener('load', () => {
-    avatarPreview.src = reader.result;
-  });
-
-  reader.readAsDataURL(file);
-});
-
-const createAdImagePreview = (src) => {
+const createAdPhotoPreview = (src) => {
   const image = document.createElement('img');
   image.src = src;
   image.classList.add('ad-form__photo');
 
   return image;
 };
+const removePreviewAvatar = () => {
+  avatarPreview.src = DEFAULT_AVATAR_URL;
+};
+const removePreviewPhoto = () => {
+  const adPhotos = adPhotoContainer.querySelectorAll('.ad-form__photo');
 
-adImageChooser.addEventListener('change', (evt) => {
-  const files = evt.target.files;
-  removePreviewPhoto();
+  for (let index = adPhotos.length - 1; index >= 0; index--) {
+    adPhotos[index].remove();
+  }
+};
 
-  for (const file of files) {
+
+const setAvatarChooserChange = () => {
+  avatarChooser.addEventListener('change', (evt) => {
+    removePreviewAvatar();
+    const file = evt.target.files[0];
+    if (!file) {
+      return;
+    }
+
     const fileName = file.name.toLowerCase();
     if (!isTypeMatche(fileName)) {
       return;
     }
 
     const reader = new FileReader();
-
     reader.addEventListener('load', () => {
-      const image = createAdImagePreview(reader.result);
-      adFormPhotoContainer.append(image);
+      avatarPreview.src = reader.result;
     });
 
     reader.readAsDataURL(file);
-  }
-});
+  });
+};
+const setAdImageChooserChange = () => {
+  adImageChooser.addEventListener('change', (evt) => {
+    removePreviewPhoto();
+    const files = evt.target.files;
+
+    for (const file of files) {
+      const fileName = file.name.toLowerCase();
+      if (!isTypeMatche(fileName)) {
+        return;
+      }
+
+      const reader = new FileReader();
+      reader.addEventListener('load', () => {
+        const image = createAdPhotoPreview(reader.result);
+        adPhotoContainer.append(image);
+      });
+
+      reader.readAsDataURL(file);
+    }
+  });
+};
+
+export {
+  removePreviewAvatar,
+  removePreviewPhoto,
+  setAvatarChooserChange,
+  setAdImageChooserChange
+};
