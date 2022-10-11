d6c8f5d9a8b149a8970d7a5744bfe4698f2b9806
qostua <qostua@gmail.com>
2022-10-11 21:22:57 +0300

    Рефакторинг и улучшение читаемости кода

--- js/activation-page.js
+++ js/activation-page.js
@@ -1,22 +1,20 @@
 const adForm = document.querySelector('.ad-form');
 const mapForm = document.querySelector('.map__filters');
 
-//activate and disable forms
 const disableAdForm = () => {
   adForm.classList.add('ad-form--disabled');
 
-  for (const fieldset of adForm.querySelectorAll('fieldset')) {
+  const fieldsets = mapForm.querySelectorAll('fieldset');
+  for (const fieldset of fieldsets) {
     fieldset.disabled = true;
   }
 };
 const disableMapForm = () => {
   mapForm.classList.add('map__filters--disabled');
 
-  for (const select of mapForm.querySelectorAll('select')) {
-    select.disabled = true;
-  }
-  for (const fieldset of mapForm.querySelectorAll('fieldset')) {
-    fieldset.disabled = true;
+  const fields = mapForm.querySelectorAll('select, fieldset');
+  for (const field of fields) {
+    field.disabled = true;
   }
 };
 const disableForm = () => {
@@ -25,7 +23,8 @@ const disableForm = () => {
 };
 
 const activateDiabledElements = (parent) => {
-  for (const disabledElement of parent.querySelectorAll('*[disabled]')) {
+  const disabledElements = parent.querySelectorAll('*[disabled]');
+  for (const disabledElement of disabledElements) {
     disabledElement.disabled = false;
   }
 };
@@ -33,7 +32,6 @@ const activateAdForm = () => {
   activateDiabledElements(adForm);
   adForm.classList.remove('ad-form--disabled');
 };
-
 const activateMapForm = () => {
   activateDiabledElements(mapForm);
   mapForm.classList.remove('map__filters--disabled');
